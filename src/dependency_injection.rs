74c4c3b6fecf8c5747928941150518b5321db039
Matias Feld <feldmatias@gmail.com>
2023-03-28 22:44:09 +0300

    add dependency injection

---
+++ src/dependency_injection.rs
@@ -0,0 +1,31 @@
+use crate::app_config::AppConfig;
+use ddi::*;
+
+struct TestService {
+    config: Service<AppConfig>,
+}
+
+impl TestService {
+    pub fn print(&self) {
+        println!("from test service {}", self.config.environment);
+    }
+}
+
+/*
+* Generate the dependency injection container.
+* Each injectable service should be registered here.
+*/
+pub fn di_container() -> ServiceProvider {
+    let mut services = ServiceCollection::new();
+    services.service(Service::new(AppConfig::new()));
+    services.service_factory(|config: &Service<AppConfig>| {
+        Ok(Service::new(TestService {
+            config: config.clone(),
+        }))
+    });
+
+    let provider = services.provider();
+    let test_service = provider.get::<Service<TestService>>().unwrap();
+    test_service.print();
+    provider
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use crate::dependency_injection::di_container;
+
 mod app_config;
+mod dependency_injection;
 
 fn main() {
     println!("Hello, world! from rust");
 
     let config = app_config::AppConfig::new();
     println!("environment: {}", config.environment);
+
+    di_container();
 
     cal_dav_fast::print_model();
 }
