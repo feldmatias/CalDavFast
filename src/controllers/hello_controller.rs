2e764c8e00ca142bd598223bb9e8af401e3735a5
Matias Feld <feldmatias@gmail.com>
2023-04-04 00:10:44 +0300

    add xml serializer and deserializer

--- src/controllers/hello_controller.rs
+++ src/controllers/hello_controller.rs
@@ -4,9 +4,26 @@ use serde::{Deserialize, Serialize};
 use crate::architecture::app_config::AppConfig;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct HelloResponse {
+pub struct XMLResponse {
     pub environment: String,
+    pub internal: Internal,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Internal {
+    #[serde(rename = "@environment")]
+    pub environment: String,
+    pub environment2: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct XMLRequest {
+    pub environment: String,
+
+    #[serde(rename = "$value")]
+    pub body: String,
+}
+
 pub struct HelloController {
     config: Service<AppConfig>,
 }
@@ -18,8 +35,14 @@ impl HelloController {
 }
 
 impl HelloController {
-    pub async fn hello(&self) -> HelloResponse {
+    pub async fn hello(&self, request: XMLRequest) -> XMLResponse {
         let env = self.config.environment.to_string();
-        HelloResponse { environment: env }
+        XMLResponse {
+            environment: request.environment,
+            internal: Internal {
+                environment: request.body,
+                environment2: env,
+            },
+        }
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,22 +1,27 @@
 use actix_web::{
-    get, middleware,
+    http::header::ContentType,
+    middleware, post,
     web::{self, Data},
     App, HttpResponse, HttpServer, Responder,
 };
+use actix_xml::Xml;
+use serde_xml_rs::to_string;
 
 use crate::{
     architecture::{
         app_config::AppConfig,
         dependency_injection::{di_container, DDIProvider},
     },
-    controllers::hello_controller::HelloController,
+    controllers::hello_controller::{HelloController, XMLRequest},
 };
 
-#[get("/")]
-async fn hello(provider: web::Data<DDIProvider>) -> impl Responder {
+#[post("/")]
+async fn hello(provider: web::Data<DDIProvider>, request: Xml<XMLRequest>) -> impl Responder {
     let controller = provider.get::<HelloController>();
-    let result = controller.hello().await;
-    HttpResponse::Ok().json(result)
+    let result = controller.hello(request.0).await;
+    HttpResponse::Ok()
+        .content_type(ContentType::xml())
+        .body(to_string(&result).unwrap())
 }
 
 pub async fn start_server() -> Result<(), std::io::Error> {
